d393709ed71fdf88b5fe338697dc2dafb019ea44
AhmadMujtaba200210 <cadeta6@gmail.com>
2023-03-17 22:54:55 +0500

    basic: different data type initialising difference

---
+++ datatypes_diff.py
@@ -0,0 +1,17 @@
+# the difference in initialising different data types
+
+# string
+x = '1,2,3'
+print(type(x))
+# list
+y = [1,2,3]
+print(type(y))
+# tuple
+z = (1,2,3)
+print(type(z))
+# set
+a = {1,2,3}
+print(type(a))
+# dict
+b = {'one':1,'two':2,'three':3}
+print(type(b))
